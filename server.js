a8ba1a59af88e0a08df823a9605f7bbbdd5c712a
favourofgod9 <divinefavourofgod9@gmail.com>
2025-04-19 07:53:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "Youokay801@gmail.com";
 const pass = "cnwupnrjfguifecl";
-// 01 APRIL
+// 04 may
 
 // Middleware
 app.use(express.json());
